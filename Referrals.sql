d6ab9c0da75c19bf46a725de273ce998db242546
nd-anjali <anjali.n@neurodiscovery.ai>
2026-03-31 16:26:08 +0530

    Schema-wise queries addition (GREENWAY)
    Added a SQL query to select referral details from the jwm schema, including various fields related to referrals and providers.

--- Referrals.sql
+++ Referrals.sql
@@ -354,3 +354,47 @@ WHERE
     OR authorization_number     IS NOT NULL;
 
 Comment:- All the tables for athenaone is same for all the schmeas so just need to change the schema name
+
+--- GREENWAY SCHEMAWISE 
+-- Schemas with all 3 tables: jwm, mind, savannah, savannah_staging
+-- jwm_staging: missing ReferralsReferringProvider
+-- mavenclad_shortlisted_mind: missing CareProviderSpecialty
+
+SELECT
+    ipc.InsPreCertID                        AS referral_id,
+    ipc.PatientID                           AS ndid,
+    ipc.VisitID                             AS encounter_id,
+    NULL                                    AS referral_type,
+    NULL                                    AS referral_direction,
+    NULL                                    AS referral_status,
+    NULL                                    AS referral_priority,
+    ipc.ReferredDate                        AS referral_date,
+    ipc.ApptDate                            AS scheduled_appointment_date,
+    NULL                                    AS appointment_id,
+    NULL                                    AS completed_date,
+    rrp.ReferringProviderID                 AS referring_provider_id,
+    NULL                                    AS referring_provider_name,
+    NULL                                    AS referred_to_provider_id,
+    NULL                                    AS referred_to_provider_name,
+    cps.Description                         AS referred_to_specialty,
+    ipc.ReferralReason                      AS referral_reason,
+    NULL                                    AS chief_complaint,
+    NULL                                    AS diagnosis_code,
+    NULL                                    AS service_procedure_code,
+    NULL                                    AS clinical_summary,
+    ipc.Description                         AS requested_service,
+    NULL                                    AS authorization_required,
+    NULL                                    AS authorization_number,
+    NULL                                    AS patient_contacted,
+    NULL                                    AS specialist_report_received,
+    NULL                                    AS specialist_report_text,
+    NULL                                    AS follow_up_required,
+    NULL                                    AS referral_loop_closed,
+    NULL                                    AS out_of_network_flag,
+    NULL                                    AS patient_declined_flag
+
+FROM jwm.InsurancePreCert ipc                       -- also: mind, savannah
+LEFT JOIN jwm.ReferralsReferringProvider rrp        -- also: mind, savannah
+    ON rrp.ReferralID = ipc.InsPreCertID
+LEFT JOIN jwm.CareProviderSpecialty cps             -- also: mind, savannah
+    ON cps.CareProviderID = rrp.ReferringProviderID;

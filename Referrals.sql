f7f4ca59286e91b252d90e7c07abc8787b2d97e9
nd-anjali <anjali.n@neurodiscovery.ai>
2026-03-31 15:57:32 +0530

    Schemawise queries

--- Referrals.sql
+++ Referrals.sql
@@ -1,3 +1,5 @@
+--Providerwise
+
 --eCW
 SELECT
     /* ====================== Identifiers ====================== */
@@ -56,6 +58,7 @@ LEFT JOIN nhxreferral nx
 
 
 --ATHENAONE
+
 SELECT
     /* ====================== Identifiers ====================== */
     ral.REFERRALID                          AS referral_id,
@@ -69,7 +72,7 @@ SELECT
     NULL                                    AS referral_priority,
     NULL                                    AS referral_date,
     NULL                                    AS scheduled_appointment_date,
-    rapl.APPOINTMENTID                      AS appointment_id,
+    ral.APPOINTMENTID                       AS appointment_id,
     NULL                                    AS completed_date,
 
     /* ====================== Referring Provider (FROM) ====================== */
@@ -77,9 +80,9 @@ SELECT
     NULL                                    AS referring_provider_name,
 
     /* ====================== Referred To Provider ====================== */
-    rauth.REFERRINGPROVIDERID               AS referred_to_provider_id,
-    rsrc.NAME                               AS referred_to_provider_name,
-    NULL                                    AS referred_to_specialty,
+    NULL                                    AS referred_to_provider_id,
+    NULL                                    AS referred_to_provider_name,
+    rauth.REFERRALAUTHSPECIALTY             AS referred_to_specialty,
 
     /* ====================== Clinical ====================== */
     NULL                                    AS referral_reason,
@@ -87,10 +90,10 @@ SELECT
     rdiag.DIAGNOSISCODE                     AS diagnosis_code,
     rproc.PROCEDURECODE                     AS service_procedure_code,
     rauth.NOTES                             AS clinical_summary,
-    NULL                                    AS requested_service,
+    rauth.NOTES                             AS requested_service,  -- RALEIGH specific
 
     /* ====================== Authorization ====================== */
-    NULL                                    AS authorization_required,
+    rauth.REFERRALAUTHNUMBER                AS authorization_required,
     COALESCE(
         rdiag.REFERRALAUTHID,
         rauth.REFERRALAUTHID,
@@ -110,9 +113,6 @@ FROM REFERRALAPPOINTMENTLINK ral
 
 LEFT JOIN REFERRALAUTHORIZATION rauth
     ON rauth.REFERRALAUTHID = ral.REFERRALAUTHID
-
-LEFT JOIN REFERRALSOURCE rsrc
-    ON rsrc.REFERRALSOURCEID = rauth.REFERRALSOURCEID
 
 LEFT JOIN REFERRALAUTHDIAGNOSISCODE rdiag
     ON rdiag.REFERRALAUTHID = rauth.REFERRALAUTHID
@@ -237,3 +237,226 @@ LEFT JOIN PatientVisitProcs pvp
 
 LEFT JOIN ORDERS o
     ON o.OrderId = pvp.OrderId;
+
+--------------------------------------------
+
+
+-- Schemawise
+
+-- RALEIGH
+-- =============================================
+SELECT
+    ral.REFERRALID                          AS referral_id,
+    NULL                                    AS ndid,
+    NULL                                    AS encounter_id,
+    NULL                                    AS referral_type,
+    NULL                                    AS referral_direction,
+    NULL                                    AS referral_status,
+    NULL                                    AS referral_priority,
+    NULL                                    AS referral_date,
+    NULL                                    AS scheduled_appointment_date,
+    ral.APPOINTMENTID                       AS appointment_id,
+    NULL                                    AS completed_date,
+    NULL                                    AS referring_provider_id,
+    NULL                                    AS referring_provider_name,
+    NULL                                    AS referred_to_provider_id,
+    NULL                                    AS referred_to_provider_name,
+    NULL                                    AS referred_to_specialty,
+    NULL                                    AS referral_reason,
+    NULL                                    AS chief_complaint,
+    rdiag.DIAGNOSISCODE                     AS diagnosis_code,
+    rproc.PROCEDURECODE                     AS service_procedure_code,
+    NULL                                    AS clinical_summary,
+    NULL                                    AS requested_service,
+    NULL                                    AS authorization_required,
+    COALESCE(rdiag.REFERRALAUTHID, rproc.REFERRALAUTHID) AS authorization_number,
+    NULL                                    AS patient_contacted,
+    NULL                                    AS specialist_report_received,
+    NULL                                    AS specialist_report_text,
+    NULL                                    AS follow_up_required,
+    NULL                                    AS referral_loop_closed,
+    NULL                                    AS out_of_network_flag,
+    NULL                                    AS patient_declined_flag,
+    'raleigh'                               AS schema_name
+FROM raleigh.REFERRALAPPOINTMENTLINK ral
+LEFT JOIN raleigh.REFERRALAUTHDIAGNOSISCODE rdiag ON rdiag.REFERRALAUTHID = ral.REFERRALID
+LEFT JOIN raleigh.REFERRALAUTHPROCEDURECODE rproc ON rproc.REFERRALAUTHID = ral.REFERRALID
+
+UNION ALL
+
+SELECT
+    rauth.REFERRALAUTHID                    AS referral_id,
+    NULL                                    AS ndid,
+    NULL                                    AS encounter_id,
+    NULL                                    AS referral_type,
+    NULL                                    AS referral_direction,
+    NULL                                    AS referral_status,
+    NULL                                    AS referral_priority,
+    NULL                                    AS referral_date,
+    NULL                                    AS scheduled_appointment_date,
+    NULL                                    AS appointment_id,
+    NULL                                    AS completed_date,
+    rauth.REFERRINGPROVIDERID               AS referring_provider_id,
+    NULL                                    AS referring_provider_name,
+    NULL                                    AS referred_to_provider_id,
+    NULL                                    AS referred_to_provider_name,
+    rauth.REFERRALAUTHSPECIALTY             AS referred_to_specialty,
+    NULL                                    AS referral_reason,
+    NULL                                    AS chief_complaint,
+    NULL                                    AS diagnosis_code,
+    NULL                                    AS service_procedure_code,
+    NULL                                    AS clinical_summary,
+    rauth.NOTES                             AS requested_service,  -- raleigh: NOTES = requested_service
+    rauth.REFERRALAUTHNUMBER                AS authorization_required,
+    rauth.REFERRALAUTHID                    AS authorization_number,
+    NULL                                    AS patient_contacted,
+    NULL                                    AS specialist_report_received,
+    NULL                                    AS specialist_report_text,
+    NULL                                    AS follow_up_required,
+    NULL                                    AS referral_loop_closed,
+    NULL                                    AS out_of_network_flag,
+    NULL                                    AS patient_declined_flag,
+    'raleigh'                               AS schema_name
+FROM raleigh.REFERRALAUTHORIZATION rauth
+
+UNION ALL
+
+-- =============================================
+-- DCND
+-- =============================================
+SELECT
+    ral.REFERRALID                          AS referral_id,
+    NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    ral.APPOINTMENTID                       AS appointment_id,
+    NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    rdiag.DIAGNOSISCODE                     AS diagnosis_code,
+    rproc.PROCEDURECODE                     AS service_procedure_code,
+    NULL, NULL, NULL,
+    COALESCE(rdiag.REFERRALAUTHID, rproc.REFERRALAUTHID) AS authorization_number,
+    NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    'dcnd'                                  AS schema_name
+FROM dcnd.REFERRALAPPOINTMENTLINK ral
+LEFT JOIN dcnd.REFERRALAUTHDIAGNOSISCODE rdiag ON rdiag.REFERRALAUTHID = ral.REFERRALID
+LEFT JOIN dcnd.REFERRALAUTHPROCEDURECODE rproc ON rproc.REFERRALAUTHID = ral.REFERRALID
+
+UNION ALL
+
+SELECT
+    rauth.REFERRALAUTHID, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    NULL, NULL,
+    rauth.REFERRINGPROVIDERID               AS referring_provider_id,
+    NULL, NULL, NULL,
+    rauth.REFERRALAUTHSPECIALTY             AS referred_to_specialty,
+    NULL, NULL, NULL, NULL,
+    rauth.NOTES                             AS clinical_summary,  -- dcnd: NOTES = clinical_summary
+    NULL,
+    rauth.REFERRALAUTHNUMBER                AS authorization_required,
+    rauth.REFERRALAUTHID                    AS authorization_number,
+    NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    'dcnd'                                  AS schema_name
+FROM dcnd.REFERRALAUTHORIZATION rauth
+
+UNION ALL
+
+-- =============================================
+-- TNCPA
+-- =============================================
+SELECT
+    ral.REFERRALID, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    ral.APPOINTMENTID, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    rdiag.DIAGNOSISCODE,
+    rproc.PROCEDURECODE,
+    NULL, NULL, NULL,
+    COALESCE(rdiag.REFERRALAUTHID, rproc.REFERRALAUTHID),
+    NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    'tncpa'                                 AS schema_name
+FROM tncpa.REFERRALAPPOINTMENTLINK ral
+LEFT JOIN tncpa.REFERRALAUTHDIAGNOSISCODE rdiag ON rdiag.REFERRALAUTHID = ral.REFERRALID
+LEFT JOIN tncpa.REFERRALAUTHPROCEDURECODE rproc ON rproc.REFERRALAUTHID = ral.REFERRALID
+
+UNION ALL
+
+SELECT
+    rauth.REFERRALAUTHID, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    NULL, NULL,
+    rauth.REFERRINGPROVIDERID,
+    NULL, NULL, NULL,
+    rauth.REFERRALAUTHSPECIALTY,
+    NULL, NULL, NULL, NULL,
+    rauth.NOTES,   -- tncpa: NOTES = clinical_summary
+    NULL,
+    rauth.REFERRALAUTHNUMBER,
+    rauth.REFERRALAUTHID,
+    NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    'tncpa'                                 AS schema_name
+FROM tncpa.REFERRALAUTHORIZATION rauth
+
+UNION ALL
+
+-- =============================================
+-- TNG-ATHENAONE
+-- =============================================
+SELECT
+    ral.REFERRALID, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    ral.APPOINTMENTID, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    rdiag.DIAGNOSISCODE,
+    rproc.PROCEDURECODE,
+    NULL, NULL, NULL,
+    COALESCE(rdiag.REFERRALAUTHID, rproc.REFERRALAUTHID),
+    NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    'tng-athenaone'                         AS schema_name
+FROM `tng-athenaone`.REFERRALAPPOINTMENTLINK ral
+LEFT JOIN `tng-athenaone`.REFERRALAUTHDIAGNOSISCODE rdiag ON rdiag.REFERRALAUTHID = ral.REFERRALID
+LEFT JOIN `tng-athenaone`.REFERRALAUTHPROCEDURECODE rproc ON rproc.REFERRALAUTHID = ral.REFERRALID
+
+UNION ALL
+
+SELECT
+    rauth.REFERRALAUTHID, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    NULL, NULL,
+    rauth.REFERRINGPROVIDERID,
+    NULL, NULL, NULL,
+    rauth.REFERRALAUTHSPECIALTY,
+    NULL, NULL, NULL, NULL,
+    rauth.NOTES,   -- tng-athenaone: NOTES = clinical_summary
+    NULL,
+    rauth.REFERRALAUTHNUMBER,
+    rauth.REFERRALAUTHID,
+    NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    'tng-athenaone'                         AS schema_name
+FROM `tng-athenaone`.REFERRALAUTHORIZATION rauth
+
+UNION ALL
+
+-- =============================================
+-- TNG_ATHENA_ONE
+-- =============================================
+SELECT
+    ral.REFERRALID, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    ral.APPOINTMENTID, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    rdiag.DIAGNOSISCODE,
+    rproc.PROCEDURECODE,
+    NULL, NULL, NULL,
+    COALESCE(rdiag.REFERRALAUTHID, rproc.REFERRALAUTHID),
+    NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    'tng_athena_one'                        AS schema_name
+FROM tng_athena_one.REFERRALAPPOINTMENTLINK ral
+LEFT JOIN tng_athena_one.REFERRALAUTHDIAGNOSISCODE rdiag ON rdiag.REFERRALAUTHID = ral.REFERRALID
+LEFT JOIN tng_athena_one.REFERRALAUTHPROCEDURECODE rproc ON rproc.REFERRALAUTHID = ral.REFERRALID
+
+UNION ALL
+
+SELECT
+    rauth.REFERRALAUTHID, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    NULL, NULL,
+    rauth.REFERRINGPROVIDERID,
+    NULL, NULL, NULL,
+    rauth.REFERRALAUTHSPECIALTY,
+    NULL, NULL, NULL, NULL,
+    rauth.NOTES,   -- tng_athena_one: NOTES = clinical_summary
+    NULL,
+    rauth.REFERRALAUTHNUMBER,
+    rauth.REFERRALAUTHID,
+    NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    'tng_athena_one'                        AS schema_name
+FROM tng_athena_one.REFERRALAUTHORIZATION rauth;
